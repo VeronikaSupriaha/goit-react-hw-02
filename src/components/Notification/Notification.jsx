e51b8c74b87550ada367474a49630da4e6f32bd1
VeronikaSupriaha <146442956+VeronikaSupriaha@users.noreply.github.com>
2024-05-04 17:30:49 +0200

    updated

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,9 @@
+import css from './Notification.module.css';
+
 export default function Notification() {
   return (
     <div>
-      <p>No feedback yet</p>
+      <p className={css.notificationText}>No feedback yet</p>
     </div>
   );
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -14,10 +14,26 @@ export default function Options({
   };
   return (
     <div className={css.btnContainer}>
-      <button onClick={() => updateFeedback('good')}>Good </button>
-      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
-      <button onClick={() => updateFeedback('bad')}>Bad</button>
-      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+      <button
+        className={css.feedbackBtn}
+        onClick={() => updateFeedback('good')}
+      >
+        Good{' '}
+      </button>
+      <button
+        className={css.feedbackBtn}
+        onClick={() => updateFeedback('neutral')}
+      >
+        Neutral
+      </button>
+      <button className={css.feedbackBtn} onClick={() => updateFeedback('bad')}>
+        Bad
+      </button>
+      {totalFeedback > 0 && (
+        <button className={css.feedbackBtn} onClick={resetFeedback}>
+          Reset
+        </button>
+      )}
     </div>
   );
 }
